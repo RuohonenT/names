1529f832fb09da390bcc97ce8e1cfed5ac3f2cfb
TRILX <ruohonen_tero@hotmail.com>
2021-02-22 14:43:48 +0200

    few tweaks, work in progress

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/api/data', (req, res) => {
